e4cad7e7463c562cafc0fb7fe17ec3404c225971
aramhart <ahartoun1@gmail.com>
2016-12-30 19:20:13 -0800

    added test show template in html view

--- client/app/shows/shows.controller.js
+++ client/app/shows/shows.controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('maxxApp')
-  .controller('ShowsCtrl', function ($scope) {
+  .controller('ShowsCtrl', function ($scope, $http) {
     $scope.message = 'Hello';
+    console.log('show view');
+
+
+   $http.get('/api/shows')
+    .success(function(data) {
+      $scope.shows = data;
+      console.log($scope.shows);
+    })
+    .error(function(err) {
+      alert('Error! Something went wrong');
+    });
+
+
   });
